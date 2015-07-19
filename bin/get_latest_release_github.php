be5cc055ec0a83f3cd1b6f5bf3b44e38f96f99e0
Thomas Seifert <thomas.seifert@mysnip.de>
2015-07-19 14:15:57 +0200

    enhanced to download to phorum.org

--- bin/get_latest_release_github.php
+++ bin/get_latest_release_github.php
@@ -1,4 +1,11 @@
 <?php
+$map = array(
+    'tarball' => 'tar.gz',
+    'zipball' => 'zip',
+);
+
+$downloadsFolder  = "/var/www/phorum/downloads/";
+$baseurlDownloads = '/downloads/';
 
 $url = "https://api.github.com/repos/phorum/Core/releases/latest";
 
@@ -11,6 +18,29 @@ curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 $data = curl_exec($ch);
 curl_close($ch);
 
-file_put_contents("/tmp/latest_release.tmp", $data);
+$release = json_decode($data);
 
+$release->tarball_url;
 
+$files = array();
+
+foreach($map as $type => $extension) {
+    $fp = fopen ($downloadsFolder . '/'.$release->tag_name.'.'.$extension, 'w+');//This is the file where we save the    information
+    $ch = curl_init($release->{$type.'_url'});//Here is the file we are downloading, replace spaces with %20
+    curl_setopt($ch, CURLOPT_TIMEOUT, 50);
+    curl_setopt($ch, CURLOPT_FILE, $fp); // write curl response to file
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_exec($ch); // get curl response
+    curl_close($ch);
+    fclose($fp);
+
+    $files[$extension]['url']  = $baseurlDownloads.$release->tag_name.'.'.$extension;
+    $files[$extension]['size'] = filesize($downloadsFolder . '/'.$release->tag_name.'.'.$extension);
+
+}
+
+$release->files = $files;
+
+$release->html = nl2br($release->body);
+
+file_put_contents("/tmp/latest_release.tmp", json_encode($release));
